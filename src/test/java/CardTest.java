affc36def2e40973ea5a564bd1aac34705b9c5d4
Вадим Дубровин <vy831158@gmail.com>
2022-06-12 18:52:07 +0300

    Pravka5

--- src/test/java/CardTest.java
+++ src/test/java/CardTest.java
@@ -21,11 +21,11 @@ public class CardTest {
 
     @BeforeEach
     public void setUp() {
-        ChromeOptions options = new ChromeOptions();
+        //ChromeOptions options = new ChromeOptions();
         //options.addArguments("--disable-dev-shm-usage");
         //options.addArguments("--no-sandbox");
         //options.addArguments("--headless");
-        driver = new ChromeDriver(options);
+        driver = new ChromeDriver();
     }
 
     @AfterEach
